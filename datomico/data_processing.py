c9c3a2eea7c9ab52aa18648b46c533aab9e05a31
Samuel <samuelnavarro@datomico.com>
2018-10-01 02:31:22 -0500

    Cambios para prueba en dif OS

--- datomico/data_processing.py
+++ datomico/data_processing.py
@@ -27,3 +27,42 @@ def convert_types(df):
             df[c] = df[c].astype(np.int32)
 
     return df
+
+
+def correct_state_ids(df, df_column=None):
+    mx_json_order = {"Entidad_Federativa":
+                     ["Aguascalientes", "Baja California",
+                      "Baja California Sur", "Campeche", "Coahuila",
+                      "Colima", "Chiapas", "Chihuahua", "Ciudad de México",
+                      "Durango", "Guanajuato", "Guerrero", "Hidalgo", "Jalisco",
+                      "México", "Michoacán", "Morelos", "Nayarit", "Nuevo León",
+                      "Oaxaca", "Puebla", "Queretaro", "Quintana Roo",
+                      "San Luis Potosí", "Sinaloa", "Sonora", "Tabasco",
+                      "Tamaulipas", "Tlaxcala",
+                      "Veracruz", "Yucatán", "Zacatecas"],
+                     "state": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+                               15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+                               27, 28, 29, 30, 31, 32]
+                     }
+    correct = pd.DataFrame.from_dict(mx_json_order)
+    df = df.merge(correct, on="Entidad_Federativa")
+    try:
+        df.drop(df_column, axis=1, inplace=True)
+    except ValueError:
+        print("Favor de proveer el nombre de la columna que \
+              contiene los ids de los estados en tu df")
+    return df
+
+
+def df_to_d3map(df, columns):
+    """
+    It gives you the df ready to put alongside mx.json fro d3
+    Parameters
+    -----------
+    df: df to process
+    columns: a list of the desired columns
+    Example:
+    df_to_d3(df, ["state", "TPV"])
+    """
+    df = df[columns]
+    return df
